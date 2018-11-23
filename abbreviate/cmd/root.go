02f7d846ea6d6459a668814ee0f870a4b40e8757
Pascal Dennerly <dnnrly@gmail.com>
2018-11-23 23:06:14 +0000

    Basic application

---
+++ abbreviate/cmd/root.go
@@ -0,0 +1,75 @@
+// Copyright © 2018 Pascal Dennerly
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	optList     = false
+	optLanguage = "en-us"
+	optSet      = "common"
+	optCustom   = ""
+	optMax      = 0
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "abbreviate [string]",
+	Short: "Shorten your string using common abbreviations",
+	Long: `This tool will attempt to shorten the string provided using common abbreviations
+specified by language and 'set'.
+
+Word boundaries will detect camel case and non-letter`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if !optList && len(args) != 1 {
+			return errors.New("requires a string to abbreviate")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		if optList {
+			// list languages and sets underneath
+			// <language 1>
+			//    - <set 1>
+			//    - <set 2>
+			// <language 2>
+			//    - <set 1>
+		}
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&optList, "list", "", optList, "List all abbreviate sets by language")
+	rootCmd.Flags().StringVarP(&optLanguage, "language", "l", optLanguage, "Language to select")
+	rootCmd.Flags().StringVarP(&optSet, "set", "s", optSet, "Abbreviation set")
+	rootCmd.Flags().StringVarP(&optCustom, "custom", "c", optCustom, "Custom abbreviation set")
+	rootCmd.Flags().IntVarP(&optMax, "max", "m", optMax, "Maximum length of string, keep on abbreviating while the string is longer than this limit")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dnnrly/abbreviate
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/pkg/errors v0.8.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+)
